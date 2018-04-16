636bb206675c5262279b73c054d3ef459614cc3c
zhengsubei <38174921+zhengsubei@users.noreply.github.com>
2018-04-16 19:41:17 +0800

    小改一下
    11

--- 常用函数/session.php
+++ 常用函数/session.php
@@ -5,8 +5,10 @@
 	// Session是在服务端保存的一个数据结构，用来跟踪用户的状态，这个数据可以保存在集群、数据库、文件中；
 	// Cookie是客户端保存用户信息的一种机制，用来记录用户的一些信息，也是实现Session的一种方式。
 
-	
-	// 第一步是开启session：  session_start();  它的作用是开启session，并随机生成一个唯一的32位的session_id
-	// 二  写入些数据  $_SESSION['hello'] = 123;
 
-?>
+
+	// 第一步是开启session：  session_start();  它的作用是开启session，并随机生成一个唯一的32位的session_id
+	// 二  写入些数据  $_SESSION['hello'] = 123、
+?> 
+
+
